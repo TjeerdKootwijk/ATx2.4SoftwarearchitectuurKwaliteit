d9076c73e54107fc2963fcea7d8a1e8d83819006
Karim  Alkichouhi <59794989+karim076@users.noreply.github.com>
2026-05-19 23:51:58 +0100

    Reverting branch fix.
    Commits should be in LegacyLink branch

--- src/main/java/com/example/atx24softwarearchitectuurkwaliteit/provider/legacylink/LegacyLinkException.java
+++
@@ -1,7 +0,0 @@
-package com.example.atx24softwarearchitectuurkwaliteit.provider.legacylink;
-
-public class LegacyLinkException extends RuntimeException {
-    public LegacyLinkException(String message) {
-        super(message);
-    }
-}
